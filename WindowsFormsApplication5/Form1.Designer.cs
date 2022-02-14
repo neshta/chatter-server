eb35f9302d9105aa4519a32f63f007f98084f745
neshta <neshta42@yandex.ru>
2022-02-14 17:21:28 +0300

    Add files via upload

---
+++ WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 25);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "Запустить сервер";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.DeepSkyBlue;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSkyBlue;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightCyan;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(179, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 25);
+            this.button2.TabIndex = 1;
+            this.button2.TabStop = false;
+            this.button2.Text = "Сервер не запущен";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 49);
+            this.textBox1.MaxLength = 5;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(53, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "5005";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Порт:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 91);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(328, 159);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication5.Properties.Resources.img_off;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(39, 39);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Подключено: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(354, 259);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ChatteR Server 1.0";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        int _clientCount = 0;
+        bool _working = false;
+        bool _stopNetwork = false;
+        const int MAX_CLIENTS = 200;
+        TcpListener _server;
+        TcpClient[] clients = new TcpClient[MAX_CLIENTS];
+
+        public Form1()
+        {
+            InitializeComponent();
+            //System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.Beep(3000, 80);
+            Console.Beep(1000, 100);
+            StartServer();
+        }
+
+        private void StartServer()
+        {
+            if (_working == false)
+            {
+                try
+                {
+                    richTextBox1.AppendText("Запуск сервера...\n");
+                    _stopNetwork = false;
+                    _clientCount = 0;
+                    int port = int.Parse(textBox1.Text);
+                    _server = new TcpListener(IPAddress.Any, port);
+                    _server.Start();
+                    Thread acceptThread = new Thread(AcceptClients);
+                    acceptThread.Start();
+                    System.Drawing.Bitmap image = WindowsFormsApplication5.Properties.Resources.img_on;
+                    pictureBox1.Image = image;
+                    button2.Text = "Остановить";
+                    button1.Text = "Сервер запущен";
+                    button2.Enabled = true;
+                    button1.Enabled = false;
+                    richTextBox1.AppendText("Сервер запущен.\n\n");
+                }
+                catch(Exception ex)
+                {
+                    richTextBox1.AppendText("При запуске сервера произошла ошибка:\n" + ex.ToString() + "\n\n");
+                    Console.Beep(3000, 80);
+                    Console.Beep(1000, 100);
+                }
+            }
+        }
+
+        private void StopServer()
+        {
+            if (_server != null)
+            {
+                richTextBox1.AppendText("Остановка сервера...\n");
+                SendToClients("ByeAll", 0);
+                Thread.Sleep(100);
+                _server.Stop();
+                _server = null;
+                _stopNetwork = true;
+
+                for (int i = 0; i < MAX_CLIENTS; i++)
+                {
+                    if (clients[i] != null) clients[i].Close();
+                }
+                System.Drawing.Bitmap image = WindowsFormsApplication5.Properties.Resources.img_off;
+                pictureBox1.Image = image;
+                button2.Text = "Сервер не запущен";
+                button1.Text = "Запустить сервер";
+                button2.Enabled = false;
+                button1.Enabled = true;
+                richTextBox1.AppendText("Сервер остановлен.\n\n");
+                _clientCount = 0;
+                Console.Beep(3000, 80);
+                Console.Beep(1000, 100);
+            }
+        }
+
+        void AcceptClients()
+        {
+            while (true)
+            {
+                try
+                {
+                    this.clients[_clientCount] = _server.AcceptTcpClient();
+                    Thread readThread = new Thread(ReceiveRun);
+                    readThread.Start(_clientCount);
+                    _clientCount++;
+                    label3.Text = "Подключено: " + _clientCount;
+
+                    if (richTextBox1.InvokeRequired) richTextBox1.Invoke(new Add((s) => richTextBox1.Text = richTextBox1.Text + s), "Запрос на подключение...\nКлиент подключен.\n");
+                    else richTextBox1.AppendText("Запрос на подключение...\nКлиент подключен.\n");
+
+                    Console.Beep(3000, 80);
+                    Console.Beep(1000, 100);
+                }
+                catch
+                {
+                    //MessageBox.Show(ex.ToString());
+                    //if (richTextBox1.InvokeRequired) richTextBox1.Invoke(new Add((s) => richTextBox1.Text = richTextBox1.Text + s), "При подключении клиента произошла ошибка.\n");
+                    //else richTextBox1.AppendText("При подключении клиента произошла ошибка.\n" + ex.ToString());
+                }
+
+
+                if (_clientCount == MAX_CLIENTS || _stopNetwork == true)
+                {
+                    break;
+                }
+                
+            }
+        }
+
+        void SendToClients(string text, int skipindex)
+        {
+            for (int i = 0; i < MAX_CLIENTS; i++)
+            {
+                if (clients[i] != null)
+                {
+                    //if (i == skipindex) continue;
+                    NetworkStream ns = clients[i].GetStream();
+                    byte[] myReadBuffer = Encoding.Unicode.GetBytes(text);
+                    ns.BeginWrite(myReadBuffer, 0, myReadBuffer.Length,
+                    new AsyncCallback(AsyncSendCompleted), ns);
+                }
+            }
+        }
+
+        public void AsyncSendCompleted(IAsyncResult ar)
+        {
+            NetworkStream ns = (NetworkStream)ar.AsyncState;
+            ns.EndWrite(ar);
+        }
+
+        void ReceiveRun(object num)
+        {
+            while (true)
+            {
+                try
+                {
+                    string s = null;
+                    StringBuilder nm = new StringBuilder(null);
+                    StringBuilder mes = new StringBuilder(null);
+                    NetworkStream ns = clients[(int)num].GetStream();
+                    //clients[(int)num].ReceiveBufferSize = 2;
+                    while (ns.DataAvailable == true)
+                    {
+                        byte[] buffer = new byte[clients[(int)num].Available];
+
+                        ns.Read(buffer, 0, buffer.Length);
+                        s += Encoding.Unicode.GetString(buffer);
+                    }
+
+                    if (s != null)
+                    {
+                        if (s.Equals("Bye"))
+                        {
+                            //try
+                            //{
+                               // clients[(int)num].Close();
+                            //}
+                            //catch { }
+                            _clientCount--;
+                            label3.Text = "Подключено: " + _clientCount;
+                        }
+                        else
+                        {
+                            Invoke(new UpdateReceiveDisplayDelegate(UpdateReceiveDisplay), new object[] { (int)num, s });
+                            s = "[" + ((int)num).ToString() + "] " + s;
+                            SendToClients(s, (int)num);
+                            s = String.Empty;
+                        }
+                    }
+                    Thread.Sleep(100);
+                }
+                catch(Exception ex)
+                {
+                    clients[(int)num].Close();
+                    if (richTextBox1.InvokeRequired) richTextBox1.Invoke(new Add((s) => richTextBox1.Text = richTextBox1.Text + s), "При подключении клиента произошла ошибка.\n" + ex.ToString());
+                    else richTextBox1.AppendText("При подключении клиента произошла ошибка.\n" + ex.ToString());
+                }
+
+
+                if (_stopNetwork == true) break;
+
+            }
+        }
+
+        public void UpdateReceiveDisplay(int clientnum, string message)
+        {
+            richTextBox1.AppendText("[" + clientnum.ToString() + "] " + message + "\n");
+        }
+
+        protected delegate void UpdateReceiveDisplayDelegate(int clientcount, string message);
+
+        delegate void Add(string text);
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StopServer();
+            label3.Text = "Подключено: 0";
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            StopServer();
+        }
+    }
+}
